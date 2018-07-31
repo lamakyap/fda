a3a36a237c5aa8550443b7c16f98265c17f97c40
Lame Jiebu <lama@rinchens-MacBook-Air.local>
2018-07-30 18:55:20 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+function search(){
+	$("#effects").html('');
+	var key = "7PcXD9iQU905xvCopWU8Nqkiy8GoiEnwpCQkcl5O"
+	var searchTerm = document.getElementById("fdaSearch").value
+
+	$.ajax({
+    
+	   	url: "https://api.fda.gov/drug/event.json?api_key="+ key + "&search=" + searchTerm+"&count=patient.reaction.reactionmeddrapt.exact",
+	   	dataType: "json",
+	   	success: function(data) {
+	   		for(i=0;i<15;i++){
+	    		var results = (data.results[i].term)
+          if(data.results[i].term === "DRUG INEFFECTIVE"){
+            results[i].term = ""
+          }else if(data.results[i].term === ""){
+            document.write("Try another search") 
+          }else{
+            		$("#effects").append(results + " " + "<br>")
+				console.log(data.results[i].term)
+          }
+	    
+			}
+	   	},
+	   type: 'GET'
+	});
+}
